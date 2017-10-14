92166bac4f0c733f89ef046884bb57f6aec622ff
guo hang <wgyhl2010@gmail.com>
2017-10-14 17:34:18 +0800

    提交新的脚本,支持带有空格的文件夹

--- Shell/dirdown.sh
+++ Shell/dirdown.sh
@@ -3,14 +3,19 @@ baidupcs set --list_page_size=10000
 olddir=$(baidupcs pwd)
 downpath=$2
 baidupcs cd $1
-files=$(baidupcs ls | awk '{a[NR]=$5}END{for(i=3;i<NR-2;i++)print a[i]}')
-for file in $files
+#files=$(baidupcs ls | awk '{a[NR]=$5}END{for(i=3;i<NR-2;i++)print a[i]}')
+baidupcs ls | awk '{for (i=5;i<=NF;i++) {printf $i" "}printf "\n"}' | awk '{a[NR]=$0}END{for(i=3;i<NR-2;i++)print a[i]}' >> swap
+cat swap | while read file
 	do 
+		if [ -z "$file" ];then
+			continue
+		fi
 		echo "start download $file"
 		if [ ! $downpath ];then
-			baidupcs download $file ${file##*/}
+			baidupcs download "$file" "${file##*/}"
 		else
-			baidupcs download $file "$2${file##*/}"
+			baidupcs download "$file" "$2${file##*/}"
 		fi
 	done
+rm -rf swap
 baidupcs cd $olddir
